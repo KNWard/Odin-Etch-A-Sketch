786e3ba20c93d35f115b2e5e4efe46e9bbec087c
Kendra <92537842+KNWard@users.noreply.github.com>
2025-03-25 12:00:23 -0400

    Added button controls. Event listeners for each button to run specific functions.

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const changeSzBtn = document.querySelector("#change-sz");
 const resetBtn = document.querySelector("#reset");
 const drawBtn = document.querySelector("#draw");
 const eraseBtn = document.querySelector("#erase");
+
+// ALL BUTTON CONTROLS
+// Button to prompt user for number input/grid size
+changeSzBtn.addEventListener("click", () => {
+ let newGrid = parseInt(prompt("Enter a number between 2 and 100"));
+ if (newGrid > 100 || !Number(newGrid)) {
+   alert("Invalid");
+ }
+ console.log(newGrid);
+ createGrid(newGrid);
+});
+
+// Sets original grid to 16x16
+let newGrid = 16;
+createGrid(newGrid);
+// Reset button sets grid back to 16x16 and background color to white
+resetBtn.addEventListener("click", () => {
+ createGrid(newGrid);
+});
+
+// Erase button
+eraseBtn.addEventListener("click", () => {
+eraseGrid(newGrid);
+})
+
+// Draw button
+drawBtn.addEventListener("click", () => {
+drawGrid(newGrid);
+})
