f3162aa2f9a9b17d489317722c4c405820d5d0a7
Kendra <92537842+KNWard@users.noreply.github.com>
2025-03-25 11:58:57 -0400

    Added variables  for container and buttons.

---
+++ script.js
@@ -0,0 +1,6 @@
+// CREATE VARIABLES FOR HTML ELEMENTS
+const gridBoard = document.querySelector("#container");
+const changeSzBtn = document.querySelector("#change-sz");
+const resetBtn = document.querySelector("#reset");
+const drawBtn = document.querySelector("#draw");
+const eraseBtn = document.querySelector("#erase");
